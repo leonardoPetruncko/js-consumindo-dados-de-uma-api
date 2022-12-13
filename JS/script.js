653bcce144fe1183992af2a4d372ac9b1d8def2c
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2022-12-13 14:48:18 -0300

    consulta de um CEP de formato válido, porém inexistente

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,11 @@
-const consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+const consultaCEP = fetch('https://viacep.com.br/ws/0100120/json/')
 .then( resposta => resposta.json())
-.then(r => console.log(r))
+.then(r => {
+    if (r.erro) {
+        throw Error('Esse cep não existe!')
+    } else
+    console.log(r)})
+.catch(erro => console.log(erro))
 
 
 console.log(consultaCEP);
