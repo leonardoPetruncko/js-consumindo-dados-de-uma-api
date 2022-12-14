c7b2178c9a5cb9f1218fc91d8022409a661e544f
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2022-12-13 21:41:55 -0300

    Trazendo resultado no formulário

--- JS/script.js
+++ JS/script.js
@@ -5,9 +5,20 @@ async function buscaEndereço(cep) {
         if (consultaCEPConvertida.erro) {
             throw Error('CEP não existente!')
         }
+        var cidade = document.getElementById('cidade')
+        var logradouro = document.getElementById('endereco')
+        var estado = document.getElementById('estado')
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+
         console.log(consultaCEPConvertida)
         return consultaCEPConvertida
     } catch (erro) {
         console.log(erro)
     }
 }
+
+var cep = document.getElementById('cep')
+cep.addEventListener("focusout", () => buscaEndereço(cep.value))
