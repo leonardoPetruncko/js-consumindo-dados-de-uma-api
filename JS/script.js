338856c467452b56053a6c38a0260fa3017bc025
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2022-12-13 21:49:32 -0300

    Mensagem de erro

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,6 @@
 async function buscaEndereço(cep) {
+    var mensagemErro = document.getElementById('erro')
+    mensagemErro.innerHTML = "";
     try {
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var consultaCEPConvertida = await consultaCEP.json()
@@ -16,6 +18,7 @@ async function buscaEndereço(cep) {
         console.log(consultaCEPConvertida)
         return consultaCEPConvertida
     } catch (erro) {
+        mensagemErro.innerHTML = `<p> CEP inválido. Tente Novamente!</p>`
         console.log(erro)
     }
 }
